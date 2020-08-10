7e9127f2d0f5b3575fa9bd82a4e7d2aecd69f784
Marina8989 <marina.mill209@gmail.com>
2020-08-10 11:50:41 -0700

    reduce

--- reduce.js
+++ reduce.js
@@ -65,7 +65,21 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+   return arr.reduce(function(accum, nexVal, ind) {
+          accum[ind][key] = value;
+         return accum;
+    }, arr);
+}
+
+ const newArr = [{ name: 'Elie' }, { name: 'Tim' }, { name: 'Matt' }, { name: 'Colt' }];
+ console.log(addKeyAndValue(newArr, 'title', 'Instructor')) 
+// [
+//     { title: 'Instructor', name: 'Elie' },
+//     { title: 'Instructor', name: 'Tim' },
+//     { title: 'Instructor', name: 'Matt' },
+//     { title: 'Instructor', name: 'Colt' }
+// ]
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
